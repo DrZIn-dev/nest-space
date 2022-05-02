0b0ae65bb80dd60a0f4e6ba84feffc0424a36589
DrZIn-dev <quick.car6020@fastmail.com>
2022-05-02 07:43:46 +0000

    chore: move s3module to s3provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigType } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
-import { S3Config } from './pkg/config/s3.config';
 import { ConfigProvider } from './pkg/provider/config.provider';
 import { LoggerProvider } from './pkg/provider/logger.provider';
 import { PostgresProvider } from './pkg/provider/postgres.provider';
-import { S3Module } from './s3/s3.module';
-/*
-  bucket: 'learn-nest-space',
-      region: 'sgp1',
-      endpoint: 'https://sgp1.digitaloceanspaces.com',
-      credentials: {
-        accessKeyId: 'FFV2XIVTRMCX5BA57X4Z',
-        secretAccessKey: 'DqGlIbe48edUEAtX7YHYuVMBHyL7gdWOjtDMWVTMrFs',
-      },
-*/
+import { S3Provider } from './pkg/provider/s3.provider';
+
 @Module({
   imports: [
     ConfigProvider,
     PostgresProvider,
     LoggerProvider,
     HealthModule,
-    S3Module.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (s3Config: ConfigType<typeof S3Config>) => ({
-        bucket: s3Config.bucket,
-        region: s3Config.region,
-        endpoint: s3Config.region,
-        credentials: {
-          accessKeyId: s3Config.key,
-          secretAccessKey: s3Config.key,
-        },
-      }),
-      inject: [S3Config.KEY],
-    }),
+    S3Provider,
   ],
   controllers: [],
   providers: [],
