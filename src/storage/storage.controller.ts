ef58758907652f6b4bb05c1f85973a379f8b2ecd
DrZIn-dev <quick.car6020@fastmail.com>
2022-05-02 12:42:34 +0000

    feat: get private file

--- src/storage/storage.controller.ts
+++ src/storage/storage.controller.ts
@@ -1,10 +1,15 @@
 import {
   Controller,
+  Get,
   Post,
+  Query,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { Readable } from 'stream';
 import { StorageService } from './storage.service';
 
 @Controller('storage')
@@ -15,5 +20,13 @@ export class StorageController {
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
     return this.storageService.uploadFile(file);
+  }
+
+  @Get('')
+  async getFile(@Query('key') key: string, @Res() res: Response) {
+    const { Body, ContentType } = await this.storageService.getFile(key);
+    const stream = Body as Readable;
+    res.set({ 'Content-Type': ContentType });
+    return stream.pipe(res);
   }
 }
--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,8 +1,11 @@
 import { S3Config } from '@/pkg/config/s3.config';
 import { S3Service } from '@/s3/s3.service';
-import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import dayjs from 'dayjs';
+import { nanoid } from 'nanoid';
+import { extname } from 'path';
 
 @Injectable()
 export class StorageService {
@@ -12,14 +15,31 @@ export class StorageService {
     private s3Config: ConfigType<typeof S3Config>,
   ) {}
 
-  async uploadFile(file: Express.Multer.File) {
-    console.log(file);
+  async uploadFile(file: Express.Multer.File): Promise<string> {
+    const key = `${nanoid(8)}${extname(file.originalname)}`;
     const command = new PutObjectCommand({
       Bucket: this.s3Config.bucket,
-      Key: file.originalname,
+      Key: key,
       Body: file.buffer,
+      ContentType: file.mimetype,
+      Metadata: {
+        ['Original-Name']: file.originalname,
+        ['Created-At']: dayjs().toISOString(),
+      },
     });
 
-    return await this.s3Service.getS3Client().send(command);
+    await this.s3Service.getS3Client().send(command);
+
+    return key;
+  }
+
+  async getFile(key: string) {
+    const command = new GetObjectCommand({
+      Bucket: this.s3Config.bucket,
+      Key: key,
+    });
+
+    const response = await this.s3Service.getS3Client().send(command);
+    return response;
   }
 }
