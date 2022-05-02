50313e7fad4be77032b2a5b5b42579d8282b3147
DrZIn-dev <quick.car6020@fastmail.com>
2022-05-02 07:41:40 +0000

    feat: s3 module

--- src/pkg/provider/config.provider.ts
+++ src/pkg/provider/config.provider.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseConfig } from '../config/database.config';
 import { GenericConfig } from '../config/generic.config';
+import { S3Config } from '../config/s3.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [DatabaseConfig, GenericConfig],
+      load: [DatabaseConfig, GenericConfig, S3Config],
     }),
   ],
   exports: [ConfigModule],
---
+++ src/s3/s3.module.ts
@@ -0,0 +1,66 @@
+import { DynamicModule, Provider } from '@nestjs/common';
+import {
+  S3ModuleAsyncOptions,
+  S3ModuleOption,
+  S3ModuleOptionsFactory,
+  S3_MODULE_CONFIG,
+} from './s3.interface';
+import { S3Service } from './s3.service';
+
+export class S3Module {
+  public static forRoot(options: S3ModuleOption): DynamicModule {
+    return {
+      module: S3Module,
+      imports: [],
+      providers: [
+        {
+          provide: S3_MODULE_CONFIG,
+          useValue: options,
+        },
+        S3Service,
+      ],
+      exports: [S3Service],
+    };
+  }
+
+  static forRootAsync(options: S3ModuleAsyncOptions): DynamicModule {
+    // But how to import other modules here and pass in the options?
+    return {
+      module: S3Module,
+      imports: [...options.imports],
+      providers: [...this.createAsyncProviders(options)],
+    };
+  }
+  private static createAsyncProviders(
+    options: S3ModuleAsyncOptions,
+  ): Provider[] {
+    if (options.useExisting || options.useFactory) {
+      return [this.createAsyncOptionsProvider(options)];
+    }
+    return [
+      this.createAsyncOptionsProvider(options),
+      {
+        provide: options.useClass,
+        useClass: options.useClass,
+      },
+    ];
+  }
+
+  private static createAsyncOptionsProvider(
+    options: S3ModuleAsyncOptions,
+  ): Provider {
+    if (options.useFactory) {
+      return {
+        provide: S3_MODULE_CONFIG,
+        useFactory: options.useFactory,
+        inject: options.inject || [],
+      };
+    }
+    return {
+      provide: S3_MODULE_CONFIG,
+      useFactory: async (optionsFactory: S3ModuleOptionsFactory) =>
+        await optionsFactory.createOptions(),
+      inject: [options.useExisting || options.useClass],
+    };
+  }
+}
